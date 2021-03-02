939bebce318ef9b0839faad7b29e99d72f1e961a
kobi levi <kobilevi503@gmail.com>
2021-03-02 19:58:56 +0200

    Add Loader & error Message components

--- frontend/react-client/src/screens/HomeScreen.jsx
+++ frontend/react-client/src/screens/HomeScreen.jsx
@@ -3,25 +3,36 @@ import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 
 function HomeScreen() {
     const dispatch = useDispatch();
 
     const productList = useSelector((state) => state.productList);
-    const { products } = productList;
+    const { loading, error, products } = productList;
 
     useEffect(() => {
         dispatch(listProducts());
     }, [dispatch]);
 
     return (
-        <Row>
-            {products.map((product) => (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={product} />
-                </Col>
-            ))}
-        </Row>
+        <>
+            <h1>Recommended Products</h1>
+            {loading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant="danger">{error}</Message>
+            ) : (
+                <Row>
+                    {products.map((product) => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product} />
+                        </Col>
+                    ))}
+                </Row>
+            )}
+        </>
     );
 }
 
