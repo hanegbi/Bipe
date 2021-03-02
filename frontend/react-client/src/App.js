dfd77d4fca193fb6e5b68da9c5e6f0253eab48b5
kobi levi <kobilevi503@gmail.com>
2021-03-02 19:54:17 +0200

    Add Header & Footer components

--- frontend/react-client/src/App.js
+++ frontend/react-client/src/App.js
@@ -2,15 +2,19 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <Router>
+            <Header />
             <main className="py-3">
                 <Container>
                     <Route path="/" component={HomeScreen} exact />
                 </Container>
             </main>
+            <Footer />
         </Router>
     );
 }
