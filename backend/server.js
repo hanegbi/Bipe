89f8644d6ee048e6bb2bddef677a820c8970fb03
kobi levi <kobilevi503@gmail.com>
2021-04-02 09:49:30 +0300

    Order route & controller for addOrderItems

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 
@@ -16,7 +17,8 @@ app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
-app.listen(5000, console.log('Server is running on port 5000.'));
+app.listen(5000, console.log("Server is running on port 5000."));
