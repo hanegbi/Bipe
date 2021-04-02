33dc4f82086aabcfdbb895826297f7df63a5255c
kobi levi <kobilevi503@gmail.com>
2021-04-02 09:56:47 +0300

    create order action & reducer

--- frontend/react-client/src/store.js
+++ frontend/react-client/src/store.js
@@ -2,13 +2,14 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productListReducer, productDetailsReducer } from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
 import {
     userLoginReducer,
     userRegisterReducer,
     userDetailsReducer,
     userUpdateProfileReducer,
 } from "./reducers/userReducers";
-import { cartReducer } from "./reducers/cartReducers";
+import { orderCreateReducer } from "./reducers/orderReducers";
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -18,17 +19,21 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducer,
 });
 
-// const cartItemsFromStorage = localStorage.getItem("cartItems")
-//     ? JSON.parse(localStorage.getItem("cartItems"))
-//     : [];
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [];
 
 const userInfoFromStorage = localStorage.getItem("userInfo")
     ? JSON.parse(localStorage.getItem("userInfo"))
     : null;
 
 const initialState = {
+    cart: {
+        cartItems: cartItemsFromStorage,
+    },
     userLogin: { userInfo: userInfoFromStorage },
 };
 
