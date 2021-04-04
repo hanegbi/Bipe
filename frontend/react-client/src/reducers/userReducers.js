7e4f9fcb799b3255e0f516ee2232da974552c0aa
ניר סורקין <nir@g-g.co.il>
2021-04-05 01:29:56 +0300

    Implement DELETE functionality

--- frontend/react-client/src/reducers/userReducers.js
+++ frontend/react-client/src/reducers/userReducers.js
@@ -17,6 +17,10 @@ import {
     USER_LIST_REQUEST,
     USER_LIST_SUCCESS,
     USER_LIST_FAILURE,
+    USER_LIST_RESET,
+    USER_DELETE_FAILURE,
+    USER_DELETE_SUCCESS,
+    USER_DELETE_REQUEST,
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
@@ -76,6 +80,7 @@ export const userUpdateProfileReducer = (state = {}, action) => {
             return state;
     }
 };
+
 export const userListReducer = (state = { users: [] }, action) => {
     switch (action.type) {
         case USER_LIST_REQUEST:
@@ -84,6 +89,22 @@ export const userListReducer = (state = { users: [] }, action) => {
             return { loading: false, users: action.payload };
         case USER_LIST_FAILURE:
             return { loading: false, error: action.payload };
+        case USER_LIST_RESET:
+            return { users: [] };
+        default:
+            return state;
+    }
+};
+
+export const userDeleteReducer = (state = {}, action) => {
+    switch (action.type) {
+        case USER_DELETE_REQUEST:
+            return { loading: true };
+        case USER_DELETE_SUCCESS:
+            return { loading: false, success: true };
+        case USER_DELETE_FAILURE:
+            return { loading: false, error: action.payload };
+
         default:
             return state;
     }
