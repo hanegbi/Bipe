22c147ae60ec4cb087f471e5252d476c624f0bb0
ניר סורקין <nir@g-g.co.il>
2021-04-14 18:22:04 +0300

    added angular chart page

--- frontend/angular-client/README.md
+++ frontend/angular-client/README.md
@@ -1,10 +1,12 @@
-# AngularClient
+# Angular + D3
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.5.
+This project demonstrates using the [D3 charting library](https://d3js.org/) for data visualization with [Angular](http://angular.io/).
 
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. You should see something like this:
+
+![D3 charts displayed in Angular](https://i.imgur.com/iXklkMQ.png)
 
 ## Code scaffolding
 
@@ -24,4 +26,4 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 
 ## Further help
 
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- frontend/angular-client/src/app/app.component.spec.ts
+++ frontend/angular-client/src/app/app.component.spec.ts
@@ -1,18 +1,14 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
-  });
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-client'`, () => {
+  it(`should have as title 'angular-d3'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-client');
+    expect(app.title).toEqual('angular-d3');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-client app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-d3 app is running!');
   });
 });
