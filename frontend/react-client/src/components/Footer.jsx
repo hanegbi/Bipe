560a3f37fd2af9803afdaa8009aee075bac09ec4
kobi levi <kobilevi503@gmail.com>
2021-04-14 14:03:43 +0300

    only admins can see amount of users

--- frontend/react-client/src/components/Footer.jsx
+++ frontend/react-client/src/components/Footer.jsx
@@ -1,28 +1,36 @@
-import {React, useEffect, useState} from "react";
+import { React, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { Container, Row } from "react-bootstrap";
 import { io } from "socket.io-client";
 
 function Footer() {
     const [userCount, setUserCount] = useState(0);
 
+    const userLogin = useSelector((state) => state.userLogin);
+    const { userInfo } = userLogin;
+
     useEffect(() => {
-        const socket = io()
-        socket.on('count', userCounter=>{
-            setUserCount(userCounter)
+        const socket = io();
+        socket.on("count", (userCounter) => {
+            setUserCount(userCounter);
         });
 
         return () => socket.disconnect();
-      }, []);
-
+    }, []);
 
     return (
         <footer>
             <Container className="d-flex flex-column align-items-center m-10">
+                {userInfo && userInfo.isAdmin && (
+                    <Row>
+                        <p className="text-center font-weight-bold">
+                            {" "}
+                            {`Connected Clients ${userCount}`}{" "}
+                        </p>
+                    </Row>
+                )}
                 <Row>
                     <p className="text-center">Copyright &copy; Bipe</p>
-                </Row>
-                <Row>
-                    <p className="text-center font-weight-bold"> {`Connected Clients ${userCount}`}  </p>
                 </Row>
             </Container>
         </footer>
