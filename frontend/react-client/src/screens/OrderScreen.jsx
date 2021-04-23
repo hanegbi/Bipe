51885fda9fd7a6b2faa19e40ecb2dacc51c4ba07
kobi levi <kobilevi503@gmail.com>
2021-04-23 11:26:42 +0300

    switch currency to nis

--- frontend/react-client/src/screens/OrderScreen.jsx
+++ frontend/react-client/src/screens/OrderScreen.jsx
@@ -77,7 +77,7 @@ function OrderScreen({ match }) {
                                                     </Link>
                                                 </Col>
                                                 <Col md={4}>
-                                                    {item.qty} x ${item.price} = $
+                                                    {item.qty} x ₪{item.price} = ₪
                                                     {addDecimals(item.qty * item.price)}
                                                 </Col>
                                             </Row>
@@ -99,28 +99,28 @@ function OrderScreen({ match }) {
                             <ListGroup.Item>
                                 <Row>
                                     <Col>Items</Col>
-                                    <Col>${order.itemsPrice}</Col>
+                                    <Col>₪{order.itemsPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
 
                             <ListGroup.Item>
                                 <Row>
                                     <Col>Shipping</Col>
-                                    <Col>${order.shippingPrice}</Col>
+                                    <Col>₪{order.shippingPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
 
                             <ListGroup.Item>
                                 <Row>
                                     <Col>Tax</Col>
-                                    <Col>${order.taxPrice}</Col>
+                                    <Col>₪{order.taxPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
 
                             <ListGroup.Item>
                                 <Row>
                                     <Col>Total</Col>
-                                    <Col>${order.totalPrice}</Col>
+                                    <Col>₪{order.totalPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
                         </ListGroup>
--- frontend/react-client/src/screens/PlaceOrderScreen.jsx
+++ frontend/react-client/src/screens/PlaceOrderScreen.jsx
@@ -84,7 +84,7 @@ function PlaceOrderScreen({ history }) {
                                                     </Link>
                                                 </Col>
                                                 <Col md={4}>
-                                                    {item.qty} x ${item.price} = $
+                                                    {item.qty} x ₪{item.price} = ₪
                                                     {item.qty * item.price}
                                                 </Col>
                                             </Row>
@@ -106,28 +106,28 @@ function PlaceOrderScreen({ history }) {
                             <ListGroup.Item>
                                 <Row>
                                     <Col>Items</Col>
-                                    <Col>${cart.itemsPrice}</Col>
+                                    <Col>₪{cart.itemsPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
 
                             <ListGroup.Item>
                                 <Row>
                                     <Col>Shipping</Col>
-                                    <Col>${cart.shippingPrice}</Col>
+                                    <Col>₪{cart.shippingPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
 
                             <ListGroup.Item>
                                 <Row>
                                     <Col>Tax</Col>
-                                    <Col>${cart.taxPrice}</Col>
+                                    <Col>₪{cart.taxPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
 
                             <ListGroup.Item>
                                 <Row>
                                     <Col>Total</Col>
-                                    <Col>${cart.totalPrice}</Col>
+                                    <Col>₪{cart.totalPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
 
