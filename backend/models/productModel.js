768527f15420e23f590f00ba6b5ed39b1d3ad1dd
kobi levi <kobilevi503@gmail.com>
2021-05-15 20:58:53 +0300

    fix product schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,22 +1,3 @@
-import mongoose from 'mongoose'
-const Schema = mongoose.Schema
-
-const productSchema = new Schema({
-    name: String,
-    price: Number,
-    image: String,
-    category: String,
-    countInStock: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-    description: String,
-})
-
-const Product = mongoose.model('Product', productSchema)
-
-export default Product
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
