17d94d9cb49acd9ed9b243d994b066c7f2fd1bad
kobi levi <kobilevi503@gmail.com>
2021-05-15 20:44:11 +0300

    update product schema with locations

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -17,3 +17,33 @@ const productSchema = new Schema({
 const Product = mongoose.model('Product', productSchema)
 
 export default Product
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    name: String,
+    image: String,
+    category: String,
+    countInStock: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    description: String,
+    locations: [
+        {
+            cityId: String,
+            branches: [
+                {
+                    id: String,
+                    price: Number,
+                },
+            ],
+            minPrice: Number,
+        },
+    ],
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
