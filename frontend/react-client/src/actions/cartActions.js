90b54e8ae7cbf133057c28f2230949212248f4ba
kobi levi <kobilevi503@gmail.com>
2021-06-10 10:38:20 +0300

    fix product fetch at add to cart

--- frontend/react-client/src/actions/cartActions.js
+++ frontend/react-client/src/actions/cartActions.js
@@ -11,10 +11,10 @@ export const addToCart = (id, qty, cityId) => async (dispatch, getState) => {
     dispatch({
         type: CART_ADD_ITEM,
         payload: {
-            product: data._id,
-            name: data.name,
-            image: data.image,
-            price: data.price,
+            product: data.product._id,
+            name: data.product.name,
+            image: data.product.image,
+            price: data.product.price,
             countInStock: data.countInStock,
             qty,
         },
